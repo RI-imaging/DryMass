67db72821a5dd43f846f93dce381ae7d9811c4a6
Paul Müller <dev@craban.de>
2018-04-23 16:37:11 +0200

    docs: always link against same version

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,7 @@
 
 # Get version number from DryMass._version file
 import mock
+import os
 import os.path as op
 import sys
 # include parent directory
@@ -30,6 +31,12 @@ pdir = op.dirname(op.dirname(op.abspath(__file__)))
 sys.path.insert(0, pdir)
 # include extenstions
 sys.path.append(op.abspath('extensions'))
+
+# RTD version
+if "READTHEDOCS_VERSION" in os.environ:
+    rtd_version = os.environ()["READTHEDOCS_VERSION"]
+else:
+    rtd_version = "latest"
 
 # Mock all dependencies
 install_requires = ["h5py", "lmfit", "numpy", "scipy", "skimage",
@@ -201,7 +208,7 @@ intersphinx_mapping = {"python": ('https://docs.python.org/', None),
                        "scipy": ('https://docs.scipy.org/doc/scipy/reference/', None),
                        "skimage": ('http://scikit-image.org/docs/stable/', None),
                        "lmfit": ('http://lmfit.github.io/lmfit-py/', None),
-                       "qpformat": ('http://qpformat.readthedocs.io/en/stable', None),
-                       "qpimage": ('http://qpimage.readthedocs.io/en/stable', None),
-                       "qpsphere": ('http://qpsphere.readthedocs.io/en/stable', None),
+                       "qpimage": ('http://qpimage.readthedocs.io/en/'+rtd_version, None),
+                       "qpsphere": ('http://qpsphere.readthedocs.io/en/'+rtd_version, None),
+                       "qpformat": ('http://qpformat.readthedocs.io/en/'+rtd_version, None),
                        }
