050515549a262ed9fe78b0ee9fc37505685ce40b
Paul Müller <dev@craban.de>
2018-07-22 21:53:12 +0200

    setup: bump package versions

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ if version.count("dev") or sys.argv.count("test"):
                     "qpimage",
                     "qpsphere"]
 else:
-    release_deps = ["qpformat==0.3.0",
-                    "qpimage==0.2.0",
-                    "qpsphere==0.2.0",
+    release_deps = ["qpformat==0.3.1",
+                    "qpimage==0.2.1",
+                    "qpsphere==0.2.7",
                     ]
 
 setup(
