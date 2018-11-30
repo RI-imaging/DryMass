a7f83c09343e8011964d849559d908835a44042a
Paul Müller <dev@craban.de>
2018-11-30 23:18:46 +0100

    turn comments in definitions.py into real strings (close #23)

--- docs/extensions/definitions_parse.py
+++ docs/extensions/definitions_parse.py
@@ -1,11 +1,10 @@
 """Parse DryMass definitions.py file"""
-import io
-import pathlib
-
 from docutils.statemachine import ViewList
 from docutils.parsers.rst import Directive
 from sphinx.util.nodes import nested_parse_with_titles
 from docutils import nodes
+
+from drymass.cli.definitions import config
 
 
 class IncludeDirective(Directive):
@@ -13,51 +12,33 @@ class IncludeDirective(Directive):
     optional_arguments = 0
 
     def run(self):
-        # determine path of definitions.py
-        here = pathlib.Path(__file__).parent
-        dfn = str(here / ".." / ".." / "drymass" / "cli" / "definitions.py")
+        sec = self.arguments[0]
 
-        with io.open(dfn, "r") as myfile:
-            text = myfile.readlines()
-
-        # extract desired section
-        idstart = '    "{}": '.format(self.arguments[0])
-        idend = '    },'
-        sec_text = []
-        in_sec = False
-        for line in text:
-            if line.startswith(idstart):
-                in_sec = True
-                continue
-            if in_sec and line.startswith(idend):
-                break
-            if in_sec:
-                sec_text.append(line.strip())
+        content = config[sec]
 
         rst = []
 
-        # parse each line to build up docstring
-        el_data = {}
-        for line in sec_text:
-            if line.startswith('"'):
-                if el_data:
-                    # append and parse
-                    parse_element(rst, **el_data)
-                    # reset data
-                    el_data = {}
-                el_data["name"] = line.split("#")[0].strip('": ')
-            elif line.startswith("("):
-                default, dtype, doc = line.split(", ", 2)
-                el_data["default"] = default[1:].strip('"')
-                el_data["dtype"] = dtype
-                el_data["doc"] = doc.strip('"),')
-            elif line.startswith("#"):
-                if "notes" not in el_data:
-                    el_data["notes"] = []
-                el_data["notes"].append(line[1:])
-        else:
-            # parse last element
-            parse_element(rst, **el_data)
+        for key in content:
+            # get data
+            if len(content[key]) == 3:
+                default, func, doc = content[key]
+                notes = None
+            elif len(content[key]) == 4:
+                default, func, doc, notes = content[key]
+            # parse dtype
+            dtype = func.__name__
+            if dtype in ["float", "int", "str"]:
+                str_dtype = ":class:`{}`".format(dtype)
+            else:
+                str_dtype = ":func:`{0} <drymass.cli.parse_funcs.{0}>`".format(
+                            dtype)
+            # add list item
+            rst.append("* | **{}** = {} ({}) -- {} ".format(key, default,
+                                                            str_dtype, doc))
+            if notes is not None:
+                # add notes
+                rst.append("  | {}".format(notes.strip()))
+            rst.append("")
 
         vl = ViewList(rst, "fakefile.rst")
         # Create a node.
@@ -66,19 +47,6 @@ class IncludeDirective(Directive):
         # Parse the rst.
         nested_parse_with_titles(self.state, vl, node)
         return node.children
-
-
-def parse_element(rst, name="", doc="", notes=[], default="", dtype=""):
-    if default == "np.nan":
-        default = "nan"
-
-    if dtype in ["float", "int", "str"]:
-        str_dtype = ":class:`{}`".format(dtype)
-    else:
-        str_dtype = ":func:`{0} <drymass.cli.parse_funcs.{0}>`".format(dtype)
-    rst.append("* | **{}** = {} ({}) -- {} ".format(name, default, str_dtype, doc))
-    if notes:
-        rst.append("  | {}".format("".join(notes).strip()))
 
 
 def setup(app):
--- drymass/cli/definitions.py
+++ drymass/cli/definitions.py
@@ -4,74 +4,74 @@ from .parse_funcs import fbool, float01, float_or_str, int_or_path, lcstr, \
 config = {
     "bg": {
         "amplitude binary threshold":
-            (None, float_or_str, "Binary image threshold value or method"),
-            # If not `None`, defines either a threshold for background
-            # segmentation or a method in :mod:`skimage.filters`.
+            (None, float_or_str, "Binary image threshold value or method",
+             "If not `None`, defines either a threshold for background "
+             "segmentation or a method in :mod:`skimage.filters`."),
         "amplitude border perc":
-            (10, float, "Amplitude bg border region to analyze [%]"),
-            # Set to 0 to disable.
+            (10, float, "Amplitude bg border region to analyze [%]",
+             "Set to 0 to disable."),
         "amplitude border px":
-            (5, int, "Amplitude bg border region to analyze [px]"),
-            # Set to 0 to disable.
+            (5, int, "Amplitude bg border region to analyze [px]",
+             "Set to 0 to disable."),
         "amplitude data":
-            (None, int_or_path, "Amplitude bg correction file or index"),
-            # Image indexing starts with 1.
-            # If a file, either specify the full path or the relative
-            # path to the directory containing the input data.
+            (None, int_or_path, "Amplitude bg correction file or index",
+             "Image indexing starts with 1. "
+             "If a file, either specify the full path or the relative "
+             "path to the directory containing the input data."),
         "amplitude mask sphere":
-            (None, float, "Circular mask for amplitude bg correction"),
-            # If not `None`, a mask is used for background correction.
-            # A value of "1.0" results in a mask with the radius as
-            # determined with the edge-detection approach from phase [sic].
-            # Set this to "1.1" to exclude peripheral phase values.
-            # If the amplitude border values or binary thresholds
-            # are set, the intersection of the resulting masks is used
-            # for background correction. For procedural details see
-            # :func:`qpsphere.cnvnc.bg_phase_mask_for_qpi`.
+            (None, float, "Circular mask for amplitude bg correction",
+             "If not `None`, a mask is used for background correction. "
+             "A value of '1.0' results in a mask with the radius as "
+             "determined with the edge-detection approach from phase [sic]. "
+             "Set this to '1.1' to exclude peripheral phase values. "
+             "If the amplitude border values or binary thresholds "
+             "are set, the intersection of the resulting masks is used "
+             "for background correction. For procedural details see "
+             ":func:`qpsphere.cnvnc.bg_phase_mask_for_qpi`."),
         "amplitude offset":
-            ("mean", lcstr, "Amplitude bg correction offset method"),
-            # Valid values are defined in
-            # :data:`qpimage.bg_estimate.VALID_FIT_OFFSETS`.
+            ("mean", lcstr, "Amplitude bg correction offset method",
+             "Valid values are defined in "
+             ":data:`qpimage.bg_estimate.VALID_FIT_OFFSETS`."),
         "amplitude profile":
-            ("tilt", lcstr, "Amplitude bg correction profile method"),
-            # Valid values are defined in
-            # :data:`qpimage.bg_estimate.VALID_FIT_PROFILES`.
+            ("tilt", lcstr, "Amplitude bg correction profile method",
+             "Valid values are defined in "
+             ":data:`qpimage.bg_estimate.VALID_FIT_PROFILES`."),
         "enabled":
-            (True, fbool, "Enable bg correction globally"),
-            # Set to *False* to disable background correction.
+            (True, fbool, "Enable bg correction globally",
+             "Set to *False* to disable background correction."),
         "phase binary threshold":
-            (None, float_or_str, "Binary image threshold value or method"),
-            # If not `None`, defines either a threshold for background
-            # segmentation or a method in :mod:`skimage.filters`.
+            (None, float_or_str, "Binary image threshold value or method",
+             "If not `None`, defines either a threshold for background ",
+             "segmentation or a method in :mod:`skimage.filters`."),
         "phase border perc":
-            (10, float, "Phase bg border region to analyze [%]"),
-            # Set to 0 to disable.
+            (10, float, "Phase bg border region to analyze [%]",
+             "Set to 0 to disable."),
         "phase border px":
-            (5, int, "Phase bg border region to analyze [px]"),
-            # Set to 0 to disable.
+            (5, int, "Phase bg border region to analyze [px]",
+             "Set to 0 to disable."),
         "phase data":
-            (None, int_or_path, "Phase bg correction file or index"),
-            # Image indexing starts with 1.
-            # If a file, either specify the full path or the relative
-            # path to the directory containing the input data.
+            (None, int_or_path, "Phase bg correction file or index",
+             "Image indexing starts with 1. "
+             "If a file, either specify the full path or the relative "
+             "path to the directory containing the input data."),
         "phase mask sphere":
-            (None, float, "Circular mask for phase bg correction"),
-            # If not `None`, a mask is used for background correction.
-            # A value of "1.0" results in a mask with the radius as
-            # determined with the edge-detection approach from phase.
-            # Set this to "1.1" to exclude peripheral phase values.
-            # If the phase border values or binary thresholds
-            # are set, the intersection of the resulting masks is used
-            # for background correction. For procedural details see
-            # :func:`qpsphere.cnvnc.bg_phase_mask_for_qpi`.
+            (None, float, "Circular mask for phase bg correction",
+             "If not `None`, a mask is used for background correction. "
+             "A value of '1.0' results in a mask with the radius as "
+             "determined with the edge-detection approach from phase. "
+             "Set this to '1.1' to exclude peripheral phase values. "
+             "If the phase border values or binary thresholds "
+             "are set, the intersection of the resulting masks is used "
+             "for background correction. For procedural details see "
+             ":func:`qpsphere.cnvnc.bg_phase_mask_for_qpi`."),
         "phase offset":
-            ("mean", lcstr, "Phase bg correction offset method"),
-            # Valid values are defined in
-            # :data:`qpimage.bg_estimate.VALID_FIT_OFFSETS`.
+            ("mean", lcstr, "Phase bg correction offset method",
+             "Valid values are defined in "
+             ":data:`qpimage.bg_estimate.VALID_FIT_OFFSETS`."),
         "phase profile":
-            ("tilt", lcstr, "Phase bg correction profile method"),
-            # Valid values are defined in
-            # :data:`qpimage.bg_estimate.VALID_FIT_PROFILES`.
+            ("tilt", lcstr, "Phase bg correction profile method",
+             "Valid values are defined in "
+             ":data:`qpimage.bg_estimate.VALID_FIT_PROFILES`."),
     },
     "holo": {
         "filter name":  # filter_name
@@ -103,18 +103,19 @@ config = {
         "eccentricity max":
             (0.7, float, "Allowed maximal eccentricity of the specimen"),
         "enabled":
-            (True, fbool, "Perform automated search for ROIs"),
-            # If set to *False*, the file "roi_slices.txt" must contain ROIs.
+            (True, fbool, "Perform automated search for ROIs",
+             "If set to `False`, the file 'roi_slices.txt' "
+             "must contain ROIs."),
         "exclude overlap px":
             (30.0, float, "Allowed distance between two objects [px]"),
         "force":
             (None, tupletupleint, "Force ROI coordinates (x1,x2,y1,y2) [px]"),
         "ignore data":
-            (None, strlist, "Exclude images and ROIs from the analysis"),
-            # To exclude individual ROIs found in a previous run, simply
-            # type the ROI index, i.e. 'exclude = 1.0, 2.2'. You may
-            # additionally exclude a full sensor image (e.g. image 3) with
-            # 'exclude = 1.0, 2.2, 3'.
+            (None, strlist, "Exclude images and ROIs from the analysis",
+             "To exclude individual ROIs found in a previous run, simply "
+             "type the ROI index, i.e. 'exclude = 1.0, 2.2'. You may "
+             "additionally exclude a full sensor image (e.g. image 3) with "
+             "'exclude = 1.0, 2.2, 3'."),
         "pad border px":
             (40, int, "Padding of object regions [px]"),
         "size variation":
@@ -122,8 +123,8 @@ config = {
     },
     "specimen": {
         "size um":
-            (10, float, "Approximate diameter of the specimen [µm]"),
-            # This is used as the initial value for the sphere analysis.
+            (10, float, "Approximate diameter of the specimen [µm]",
+             "This is used as the initial value for the sphere analysis."),
     },
     "sphere": {
         "edge coarse":
@@ -155,16 +156,16 @@ config = {
         "image verbosity":  # verbose
             (1, int, "Verbosity level of image fitting algorithm"),
         "method":
-            ("edge", lcstr, "Method for determining sphere parameters"),
-            # Valid values are *edge* (edge-detection approach) or
-            # *image* (2D phase image fitting).
+            ("edge", lcstr, "Method for determining sphere parameters",
+             "Valid values are 'edge' (edge-detection approach) or "
+             "'image' (2D phase image fitting)."),
         "model":
-            # Valid values are defined in
-            # :data:`qpsphere.models.available`. If *method=edge*, then
-            # *model* must be set to *projection*. If *method=image*,
-            # setting *model* to *rytov-sc* has the best trade-off between
-            # accuracy and speed.
-            ("projection", lcstr, "Physical sphere model"),
+            ("projection", lcstr, "Physical sphere model",
+             "Valid values are defined in "
+             ":data:`qpsphere.models.available`. If `method=edge`, then "
+             "`model` must be set to `projection`. If `method=image`, "
+             "setting `model` to `rytov-sc` has the best trade-off between "
+             "accuracy and speed."),
         "refraction increment":
             (0.18, float, "Refraction increment [mL/g]"),
         "radial inclusion factor":
