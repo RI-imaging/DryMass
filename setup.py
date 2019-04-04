dae185bd401c4da3fb5447c9cd237759a4abe59a
Paul Müller <dev@craban.de>
2019-04-04 09:38:00 +0200

    update changelog, bump dependencies

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ if version.count("post") or sys.argv.count("test"):
                     "qpimage",
                     "qpsphere"]
 else:
-    release_deps = ["qpformat==0.9.0",
-                    "qpimage==0.5.0",
-                    "qpsphere==0.5.0",
+    release_deps = ["qpformat==0.10.0",
+                    "qpimage==0.5.2",
+                    "qpsphere==0.5.1",
                     ]
 
 setup(
